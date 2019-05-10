e2423a286d593b476acaeda4b00bc205f60424a5
rakibtg <rakibtg@gmail.com>
2019-05-11 00:50:44 +0600

    removes broken images

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let items = document.getElementsByClassName( 'job-title-text' )
 for (let index = 0; index < items.length; index++) {
     const item = items[index].children[0];
-    item.setAttribute( 'href', 'http://jobs.bdjobs.com/'+item.getAttribute( 'href' ) )
+    item.setAttribute( 'href', 'http://jobs.bdjobs.com/'+item.getAttribute( 'href' ) )  
+}
+
+let images = document.querySelectorAll('img')
+for (let index = 0; index < images.length; index++) {
+    images[index].remove();
 }
